4d56bd6c69b53ea0ef861fceb399ada35d226172
Nimisha500 <64682800+Nimisha500@users.noreply.github.com>
2021-07-06 15:57:58 +0530

    Add files via upload

---
+++ Reshape the matrix.py
@@ -0,0 +1,51 @@
+"""
+In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+
+You are given an m x n matrix mat and two integers r and c representing the row number and column number of the wanted reshaped matrix.
+
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
+Example 2:
+
+
+Input: mat = [[1,2],[3,4]], r = 2, c = 4
+Output: [[1,2],[3,4]]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+-1000 <= mat[i][j] <= 1000
+1 <= r, c <= 300
+"""
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m=len(mat)
+        n=len(mat[0])
+        mat2=[]
+        if(m*n!=r*c):
+            mat2=mat
+        else:
+            x=0
+            y=0
+            mat2=[[0 for i in range(c)]for j in range(r)]
+            for i in range(m):
+                for j in range(n):
+                    mat2[x][y]=mat[i][j]
+                    y=y+1
+                    if y==c:
+                        y=0
+                        x=x+1
+                    
+        return mat2
